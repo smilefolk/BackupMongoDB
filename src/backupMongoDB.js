952d63a98191de1c2739a3ff68f8a5c0c5f8a324
Pipat Thammakosol <smilefolkk@gmail.com>
2018-08-01 12:08:29 +0700

    fixed

--- src/backupMongoDB.js
+++ src/backupMongoDB.js
@@ -1,24 +1,24 @@
 var cmd = require('node-command-line'),
-    Promise = require('bluebird');
-var CronJob = require('cron').CronJob;
-var config = require('config');
-var moment = require('moment-timezone');
-moment.tz('Asia/Bangkok').format();
+  Promise = require('bluebird')
+var CronJob = require('cron').CronJob
+var config = require('config')
+var moment = require('moment-timezone')
+moment.tz('Asia/Bangkok').format()
 const commandLines = config.get('commandLines')
 var job = new CronJob({
-    cronTime: '0 0 18 * * *',
-    onTick: function () {
-        console.log('running');
-        commandLines.forEach(commandLine => {
-            runSingleCommandWithoutWait(commandLine.cmd)
-        });
-    },
-    start: false,
-    timeZone: 'Asia/Bangkok'
-});
-job.start();
+  cronTime: '0 15 12 * * *',
+  onTick: function() {
+    console.log('running')
+    commandLines.forEach(commandLine => {
+      runSingleCommandWithoutWait(commandLine.cmd)
+    })
+  },
+  start: false,
+  timeZone: 'Asia/Bangkok',
+})
+job.start()
 
 function runSingleCommandWithoutWait(commandLine) {
-    cmd.run(commandLine + `${moment().format('YYYYMMDD')}`);
-    console.log('Executed your command :)');
+  cmd.run(commandLine + `${moment().format('YYYYMMDD')}`)
+  console.log('Executed your command :)')
 }
