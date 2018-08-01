2346a33bfc7caf62221243932979c1228b575355
Pipat Thammakosol <smilefolkk@gmail.com>
2018-08-01 12:41:33 +0700

    fixed

--- src/backupMongoDB.js
+++ src/backupMongoDB.js
@@ -6,11 +6,12 @@ var moment = require('moment-timezone')
 moment.tz('Asia/Bangkok').format()
 const commandLines = config.get('commandLines')
 var job = new CronJob({
-  cronTime: '0 15 12 * * *',
+  cronTime: '0 45 12 * * *',
   onTick: function() {
     console.log('running')
     commandLines.forEach(commandLine => {
       runSingleCommandWithoutWait(commandLine.cmd)
+      runCopyFileCommandWithoutWait(commandLine.copy)
     })
   },
   start: false,
@@ -20,5 +21,10 @@ job.start()
 
 function runSingleCommandWithoutWait(commandLine) {
   cmd.run(commandLine + `${moment().format('YYYYMMDD')}`)
+  console.log('Executed your command :)')
+}
+
+function runCopyFileCommandWithoutWait(commandLine) {
+  cmd.run(commandLine + `${moment().format('YYYYMMDD')} /backup/mongodb/`)
   console.log('Executed your command :)')
 }
